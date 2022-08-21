e287c8e74c425ad6e1b4dd6098de076417e223f0
SUBHAN <lionsubhan123@gmail.com>
2022-08-21 05:02:05 +0500

    Responsive Sign-in

--- resources/views/Frontend/includes/header.blade.php
+++ resources/views/Frontend/includes/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-light fixed-top main-menu">
     <div class="container-fluid mx-5">
-        <a class="navbar-brand logo" href="#">
+        <a class="navbar-brand logo" href="/">
             <img src="assets/images/logo.png" alt="">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
@@ -10,7 +10,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0 menu-links">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">About Us</a>
@@ -66,11 +66,13 @@
         </div>
         <div id="mySidenav" class="sidenav" >
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav()" style="color: var(--black);">&times;</a>
-                <a href="/index.html" >Home</a>
-                <a href="/menu.html" >Menu & Pricing</a>
-                <a href="/openingtime.html">Opening Hours</a>
-                <a href="/contact.html" >Contact</a>
-                <a href="/aboutus.com" >About Us</a>
+                <a href="/" >Home</a>
+                <a href="/ecommerce-signin" >My Account</a>
+                <a href="/ecommerce-signup" >Join Now</a>
+                <a href="#" >Pricing</a>
+                <a href="/openingtime.html">Shop</a>
+                <a href="#" >Contact</a>
+                <a href="#" >About Us</a>
           </div>
           <span style="font-size:30px;cursor:pointer;float: right;color:var(--black); " class="bars" onclick="openNav()">&#9776; </span>
           
--- resources/views/sign-in.blade.php
+++ resources/views/sign-in.blade.php
@@ -6,9 +6,9 @@
                 @csrf
                 <h2 style="padding: 95px 0px 10px 0px;font-weight: 900;letter-spacing:1px;text-align: center;">Sign In</h2>
                 <label for="email">E-MAIL</label><br>
-                <input type="email" placeholder="Email Address" name="email" id="email"><br>
+                <input type="email" class="sign-in-input" placeholder="Email Address" name="email" id="email"><br>
                 <label for="password">PASSWORD</label><br>
-                <input type="text" placeholder="Enter Password" name="password" id="password"><br>
+                <input type="text" class="sign-in-input" placeholder="Enter Password" name="password" id="password"><br>
                 <input type="submit" id="submit-btn" value="Sign In">
                 <a href="/ecommerce-signup" class="sign_in" style="color: var(--dark-grey);">Sign Up<i
                         class="fa-solid fa-arrow-right-long" style="margin-left: 10px;"></i></a>
@@ -16,4 +16,4 @@
         </div>
     </div>
 
-@end
+@endsection
